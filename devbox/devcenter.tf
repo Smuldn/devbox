5704b11c9bb2697070e6fbddbea47fc1e3c7c5c8
Nathan Smulders <nathan.smulders@ns.nl>
2025-03-12 15:24:01 +0100

    Update Terraform backend configuration with specific storage account and container names

--- devbox/devcenter.tf
+++ devbox/devcenter.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "azurerm" {
         resource_group_name   = "tfstate"
-        storage_account_name  = "tfstate-storage-account"
-        container_name        = "yourcontainer"
+        storage_account_name  = "satfstate120325"
+        container_name        = "tfstate"
         key                   = "terraform.tfstate"
     }
 }
